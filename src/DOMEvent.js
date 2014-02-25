99fb4fd6f0cfe418fd6a8859c97dc879486defc0
Air <baishuiz@gmail.com>
2014-02-25 10:16:00 -0500

    split EventStructure

--- src/DOMEvent.js
+++ src/DOMEvent.js
@@ -7,35 +7,7 @@
     var base = beacon.base;
     var host = (function(){return this}());
     
-    var EventStructure  = function(dom) {
-       var arrayIndexOf = base.arrayIndexOf;
-       var events = [];
-       var api = {
-           dom : dom
-          ,attachEvent : function (eventName, eventHandle) {
-              events[eventName] = events[eventName] || [];
-              events[eventName].push(eventHandle);
-              events.push(eventName);
-          }
-          
-         ,removeEvent : function (eventName, eventHandle) {
-              var eventHandles = events[eventName];
-              var result;
-              if(eventName && eventHandle) {
-                  var handleIndex = arrayIndexOf(eventHandles, eventHandle);
-                  result = events[eventName].splice(handleIndex, 1);
-              } else if(eventName && !eventHandle) {
-                  result = events[eventName];
-                  events[eventName] = [];
-              } else if(!eventName && !eventHandle) {
-                  result = events;
-                  events = [];
-              }
-              return result;
-          } 
-       }
-       return api
-    }
+    var EventStructure  = base.EventStructure;
 
     var eventMap = {
         structures : []
